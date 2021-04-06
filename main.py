dda9283725b2188c59d0c6b132d824c5911f1d0b
Boshi Yuan <542629877@qq.com>
2021-04-06 17:00:10 +0800

    Minor modification

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from crypto_names import crypto_names
 from timeout import timeout
 from colored_logger import ColoredFormatter
 
+
 def write_result(ana: AnalyseApkCrypto, csv_java, csv_elf):
     for crypto_name, class_dict in ana.classes_with_crypto.items():
         for class_name, methods in class_dict.items():
@@ -23,6 +24,7 @@ def write_result(ana: AnalyseApkCrypto, csv_java, csv_elf):
         csv_elf.writerow([ana.app_name, ana.package_name, result.elf_name] 
         + list(result.symbol_table_with_crypto_name.values())
         + list(result.crypto_constants_results.values()))
+
 
 @timeout(300)
 def analyse_and_write_result(apk_file, csv_java, csv_elf):
@@ -43,10 +45,12 @@ if __name__ == '__main__':
 
     logger = logging.getLogger('AndroidCryptoDetection')
     logger.setLevel(logging.DEBUG)
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(ColoredFormatter())
-    logger.addHandler(stream_handler)
-    logger.addHandler(logging.FileHandler(os.path.join(path, 'analyse_log.log')))
+    handler = logging.StreamHandler()
+    handler.setFormatter(ColoredFormatter())
+    logger.addHandler(handler)
+    handler = logging.FileHandler(os.path.join(path, 'analyse_log.log'))
+    handler.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', '%H:%M:%S'))
+    logger.addHandler(handler)
 
     with open(os.path.join(path, 'result_java.csv'), 'w', newline='') as f_java, open(os.path.join(path, 'result_elf.csv'), 'w', newline='') as f_elf:
         csv_java = csv.writer(f_java)
